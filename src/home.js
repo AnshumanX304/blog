2afd9a92bf1bf29380b0daaf3af97738d4f207c6
Anshuman Tiwari <anshuman21153045@akgec.ac.in>
2022-12-02 13:42:05 +0530

    data fetching from json web server enabled

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import{useState} from 'react';
+import{useState,useEffect} from 'react';
 import Bloglist from './bloglist';
 const Home = () => {
     // const [name,setName]=useState('mario');
@@ -6,15 +6,38 @@ const Home = () => {
     // const handleClick=()=>{
     //     setName('luigi');
     //     setAge(30);
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ]);
+    const [blogs, setBlogs] = useState(null);
+
+    //   const  [name,setName]=useState('mario');
+
       const handleDelete=(id)=>{
         const newBlogs=blogs.filter(blog=>blog.id!==id);
         setBlogs(newBlogs);
       }
+// useEffect(()=>{
+//     console.log('use effect ran !');
+//     console.log(blogs);    
+// })  //here useEffect hook function will run everytime a change is made in the
+// //blogs array via useState hook
+
+useEffect(()=>{
+   fetch('http://localhost:8000/blogs')
+   .then(res=>{
+        return res.json();
+   })
+   .then(data=>{
+        setBlogs(data);
+   })
+
+},[]) //here empty array is passed as a dependency so that this function 
+//runs only on the initial render
+
+// useEffect(()=>{
+//     console.log('use effect ran !');
+//     console.log(name);   
+
+// },[name]) //Here name becomes a dependency and the function runs only for initial render or
+//         //when name is changed
     return ( 
         <div className="home">
             {/* <h1>Home page content</h1>
@@ -26,7 +49,9 @@ const Home = () => {
                     <p>Written by { blog.author }</p>
                 </div>
             ))} */}
-            <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            {blogs && <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+            {/* <button onClick={()=>setName('luigi')}>Click me</button>//button to 
+            //run useEffect function when name is changed  */}
         </div>
     );
 }
